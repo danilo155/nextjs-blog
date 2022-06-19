9675348e99d775997abe6d8ee5c213c35a4abeab
данило <playgroop@ukr.net>
2022-06-19 12:56:25 +0300

    Добавление заголовка

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,7 +7,9 @@ export default function Home() {
         <title>My Pages</title>
         <meta href='../public/favicon.ico' />
       </Head>
-
+      <main>
+        <h1>Hello World</h1>
+      </main>
       <style jsx global>{`
         html,
         body {
